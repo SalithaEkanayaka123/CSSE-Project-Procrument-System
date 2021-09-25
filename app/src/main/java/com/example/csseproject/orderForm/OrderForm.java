6a164aaf57fdea735df84c2fc2be2d0bed1afdb1
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-25 15:20:08 +0530

    Added the Activities.

---
+++ app/src/main/java/com/example/csseproject/orderForm/OrderForm.java
@@ -0,0 +1,16 @@
+package com.example.csseproject.orderForm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.csseproject.R;
+
+public class OrderForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_form);
+    }
+}
---
+++ app/src/main/java/com/example/csseproject/orderStatus/OrderStatus.java
@@ -0,0 +1,16 @@
+package com.example.csseproject.orderStatus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.csseproject.R;
+
+public class OrderStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_status);
+    }
+}
