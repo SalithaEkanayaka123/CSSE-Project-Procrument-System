ee61153bbf9a62eca29d1903100e3933a9b46715
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-25 15:04:51 +0530

    init commitment backend desktop application

---
+++ src/main/java/lk/csse/procurement/backend/csse/CsseApplication.java
@@ -0,0 +1,13 @@
+package lk.csse.procurement.backend.csse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/csse/procurement/backend/csse/CsseApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.csse.procurement.backend.csse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
