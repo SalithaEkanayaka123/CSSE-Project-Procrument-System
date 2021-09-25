ddb12021dc156fd662a3393393a68f4b3709f8b8
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-25 15:17:58 +0530

    Created the Development Index Page for the Mobile Application.

--- app/src/main/java/com/example/csseproject/IndexPage.java
+++ app/src/main/java/com/example/csseproject/IndexPage.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class IndexPage extends AppCompatActivity {
     //
 
     @Override
