df36eda3c75af05ad7a828ad2a14105a3bfc24d8
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-28 05:08:21 +0530

    Stage for today

---
+++ app/src/main/java/com/example/csseproject/ProfileView/LoginPage.java
@@ -0,0 +1,16 @@
+package com.example.csseproject.ProfileView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.csseproject.R;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
