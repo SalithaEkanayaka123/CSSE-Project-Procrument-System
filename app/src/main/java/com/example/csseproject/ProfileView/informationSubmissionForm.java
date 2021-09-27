8804470428e9c0e8f855adcb77b3d4df1446f99b
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-28 03:42:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/csseproject/ProfileView/informationSubmissionForm.java
@@ -0,0 +1,16 @@
+package com.example.csseproject.ProfileView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.csseproject.R;
+
+public class informationSubmissionForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_submission_form);
+    }
+}
