8532fee1f02a8216847b40540d2507c0514a061f
Taneesha <taneesha.chandrasena@acentura.com>
2021-09-28 16:34:14 +0530

    add drop down

--- src/components/UserManagment/UserManagmentForm.jsx
+++ src/components/UserManagment/UserManagmentForm.jsx
@@ -2,12 +2,7 @@ import React, { useState, useEffect } from "react";
 import {
     Grid,
     FormControl,
-    FormLabel,
-    RadioGroup,
-    FormControlLabel,
-    Radio,
-    Box,
-    TextField, makeStyles,
+    TextField, makeStyles, InputLabel, Select, MenuItem,
 } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
@@ -26,22 +21,38 @@ const UserManagementForm = () => {
         <form className={classes.root}>
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <TextField id="outlined-basic" label="User name" variant="outlined" />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <TextField id="outlined-basic" label="Phone number" variant="outlined" />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <TextField id="outlined-basic" label="First Name" variant="outlined" />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <FormControl variant="outlined" className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-outlined-label"
+                            id="demo-simple-select-outlined"
+                            // value={age}
+                            // onChange={handleChange}
+                            label="Age"
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <TextField id="outlined-basic" label="Last Name" variant="outlined" />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <TextField id="outlined-basic" label="Password" variant="outlined" type="password"/>
                 </Grid>
             </Grid>
         </form>
