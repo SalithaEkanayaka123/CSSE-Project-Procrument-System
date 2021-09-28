c81ce5a7f2573595daecde03609e71072a68c929
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-28 21:58:41 +0530

    Order Status Page.

--- app/src/main/java/com/example/csseproject/IndexPage.java
+++ app/src/main/java/com/example/csseproject/IndexPage.java
@@ -9,21 +9,32 @@ import android.widget.Button;
 
 import com.example.csseproject.HomePage.HomePage;
 import com.example.csseproject.HomePage.Model.OrderModel;
+import com.example.csseproject.OrderDetails.OrderStatus;
 
 public class IndexPage extends AppCompatActivity {
 
     Button settingButton;
+    Button orderStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         settingButton = findViewById(R.id.order_list);
+        orderStatus = findViewById(R.id.view_status);
 
         settingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), HomePage.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
+        orderStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), OrderStatus.class);
                 view.getContext().startActivity(intent);
             }
         });
