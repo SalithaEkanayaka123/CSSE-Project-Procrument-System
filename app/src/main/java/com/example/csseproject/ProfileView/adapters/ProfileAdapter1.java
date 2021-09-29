571375e741a3f4d72457575648a0e51c2c60d78f
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-29 16:55:52 +0530

    adapter added chnage if needed
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.com>

---
+++ app/src/main/java/com/example/csseproject/ProfileView/adapters/ProfileAdapter1.java
@@ -0,0 +1,16 @@
+package com.example.csseproject.ProfileView.adapters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.csseproject.R;
+
+public class ProfileAdapter1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_adapter1);
+    }
+}
