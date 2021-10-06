81c7b3e6000af8d778cb689f32d5759a4edd5b43
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2021-10-07 00:26:48 +0530

    backend updated

---
+++ src/main/java/lk/csse/procurement/backend/controller/PaymentController.java
@@ -0,0 +1,53 @@
+package lk.csse.procurement.backend.controller;
+
+import lk.csse.procurement.backend.model.Card;
+import lk.csse.procurement.backend.model.Cash;
+import lk.csse.procurement.backend.model.Check;
+import lk.csse.procurement.backend.model.Order;
+import lk.csse.procurement.backend.repository.OrderRepository;
+import lk.csse.procurement.backend.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
+public class PaymentController {
+    @Autowired
+    PaymentRepository paymentRepository;
+
+
+    @PostMapping("/paymentCard")
+    public ResponseEntity<?> cardPayment(@RequestBody Card card){
+        try {
+            paymentRepository.save(card);//must be changed
+            return new ResponseEntity<Card>(card, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/paymentCash")
+    public ResponseEntity<?> cashPayment(@RequestBody Cash cash){
+        try {
+            paymentRepository.save(cash);//must be changed
+            return new ResponseEntity<Cash>(cash, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/paymentCheque")
+    public ResponseEntity<?> ChequePayment(@RequestBody Check check){
+        try {
+            paymentRepository.save(check);//must be changed
+            return new ResponseEntity<Check>(check, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+}
